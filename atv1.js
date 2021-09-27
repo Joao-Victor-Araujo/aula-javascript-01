2ee7ac205a6952c4b79e3aa1c1f1907365197716
unknown <jvas1999@gmail.com>
2021-09-27 19:40:45 -0300

    up das atv

---
+++ atv1.js
@@ -0,0 +1,24 @@
+//Atividade de Fixação
+
+//a) 
+var number = 50;
+
+//b)
+var string = "texto";
+
+//c)
+var Verdade = true;
+var Mentira = false;
+
+//d)
+var DiasdaSemana = ["segunda", "terça", "quarta", "quinta", "sexta", "sabado", "domingo"];
+
+//e)
+var carro = {year: 2020, color: "green", model: "Gol"};
+
+//f)
+var value_undefined;
+
+//g)
+
+var value_empty = null;
---
+++ atv2.js
@@ -0,0 +1,14 @@
+//1
+
+var qlqcoisa = 0;
+var kayoFeio = 1;
+
+//2
+
+let letsgo = 1;
+let naoQuero = 0;
+
+//3
+
+const boraqbora = 2.4
+const pi = 3.14
